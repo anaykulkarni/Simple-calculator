8c09f736b3fc383486828f5d37c0cda24f72d7f8
Anay <Anay@DESKTOP-7SAPBQ6>
2018-05-31 16:06:17 +0530

    All features were added to the Calculator class and 20 test cases released in the CalculatorTest class

---
+++ simple calculator/src/calculator/Calculator.java
@@ -0,0 +1,80 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author Anay
+ */
+public class Calculator {
+    
+    double operand1 = 0.0;
+    double operand2 = 0.0;
+    String result = "";
+    Scanner scan = new Scanner(System.in);
+    
+    public void getData(){
+        System.out.println("Enter the first operand: ");
+        operand1 = scan.nextDouble();
+        System.out.println("Enter the second operand: ");
+        operand2 = scan.nextDouble();
+        selectOperation(operand1, operand2);
+    }
+    
+    public void selectOperation(double num1, double num2){
+        int choice;
+        choice = scan.nextInt();
+        switch(choice){
+            case 1:
+                result = Double.toString(this.add(num1, num2));
+                break;
+            case 2:
+                result = Double.toString(this.subtract(num1, num2));
+                break;
+            case 3:
+                result = Double.toString(this.multiply(num1, num2));
+                break;
+            case 4:
+                this.divide(num1, num2);
+                break;
+            case 5: 
+                return;
+            default: System.out.println("You entered an invalid choice!");
+        }
+    }
+    
+    public double add(double num1, double num2){
+        return (num1 + num2);
+    }
+
+    public double subtract(double num1, double num2) {
+        return (num1 - num2);
+    }
+
+    public double multiply(double num1, double num2) {
+        return (num1 * num2);
+    }
+
+    public double divide(double num1, double num2) {
+        if((num1 == 0) && (num2 == 0))
+            result = "ERROR: Indertiminate form!";      /* stores a message in the result and prevents 0/0 */
+        else if(num2 == 0)
+            result = "ERROR: Cannot divide by zero!";   /* stores a message in the result and prevents divide by zero */ 
+        else{
+            result = Double.toString((num1 / num2));    /* stores the result of division as a string */
+            return (num1/num2);
+        }   
+        return 0;                                       /* returns zero indicating incorrect division was prevented.
+                                                           In this case, the returned result is not stored anywhere */
+    }
+    
+    public String getResult(){
+        return result;
+    }
+   
+}
---
+++ simple calculator/test/calculator/CalculatorTest.java
@@ -0,0 +1,147 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package calculator;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Anay
+ */
+public class CalculatorTest {
+    
+    Calculator calc;
+    
+    public CalculatorTest() {
+    }
+    
+    @BeforeClass
+    public static void setUpClass() {
+    }
+    
+    @AfterClass
+    public static void tearDownClass() {
+    }
+    
+    @Before
+    public void setUp() {
+        calc = new Calculator();
+    }
+    
+    @After
+    public void tearDown() {
+        
+    }
+
+    @Test
+    public void testAdd1() {
+        Assert.assertEquals(20.0, calc.add(15.0, 5.0), 0.0000001);
+    }
+    
+    @Test
+    public void testAdd2() {
+        Assert.assertEquals(20000.4195, calc.add(12345.2348, 7655.1847), 0.0000001);
+    }
+    
+    @Test
+    public void testAdd3() {
+        Assert.assertEquals(-10.0, calc.add(-15.0, 5.0), 0.0000001);
+    }
+    
+    @Test
+    public void testAdd4() {
+        Assert.assertEquals(0.0, calc.add(15.0, -15.0), 0.0000001);
+    }
+    
+    @Test
+    public void testAdd5() {
+        Assert.assertEquals(-20.0, calc.add(-15.0, -5.0), 0.0000001);
+    }
+    
+    @Test
+    public void testSub1() {
+        Assert.assertEquals(10.0, calc.subtract(15.0, 5.0), 0.0000001);
+    }
+    
+    @Test
+    public void testSub2() {
+        Assert.assertEquals(4690.0501, calc.subtract(12345.2348, 7655.1847), 0.0000001);
+    }
+    
+    @Test
+    public void testSub3() {
+        Assert.assertEquals(-20.0, calc.subtract(-15.0, 5.0), 0.0000001);
+    }
+    
+    @Test
+    public void testSub4() {
+        Assert.assertEquals(30.0, calc.subtract(15.0, -15.0), 0.0000001);
+    }
+    
+    @Test
+    public void testSub5() {
+        Assert.assertEquals(-10.0, calc.subtract(-15.0, -5.0), 0.0000001);
+    }
+    
+    @Test
+    public void testMultiply1() {
+        Assert.assertEquals(75.0, calc.multiply(-15.0, -5.0), 0.0000001);              
+    }
+    
+    @Test
+    public void testMultiply2() {
+        Assert.assertEquals(0.0, calc.multiply(-15.0, 0), 0.0000001);              
+    }
+    
+    @Test
+    public void testMultiply3() {
+        Assert.assertEquals(698262.575, calc.multiply(2845.0, 245.435), 0.0000001);              
+    }
+    
+    @Test
+    public void testMultiply4() {
+        Assert.assertEquals(-75.0, calc.multiply(15.0, -5.0), 0.0000001);              
+    }
+    
+    @Test
+    public void testMultiply5() {
+        Assert.assertEquals(0.0, calc.multiply(0.0, 0.0), 0.0000001);              
+    }
+    
+    @Test
+    public void testDivide1() {
+        Assert.assertEquals(1.492218, calc.divide(4.1230, 2.7630), 0.000001);             
+    }
+    
+    @Test
+    public void testDivide2() {
+        Assert.assertEquals(-140.0, calc.divide(-420.0, 3.0), 0.0000001);
+    }
+    
+    @Test
+    public void testDivide3() {
+        Assert.assertEquals(0.0, calc.divide(0.0, 23753.245), 0.0000001);                      
+    }
+    
+    @Test
+    public void testDivide4() {
+        if(Double.isInfinite(calc.divide(1.0, 0.0)))
+            fail();       
+    }
+    
+    @Test
+    public void testDivide5() {
+        if( Double.isNaN(calc.divide(0.0, 0.0)) )
+            fail();
+    }
+    
+}
